87175282b3ce27f0c81e733d4169f5222b8ecf3a
Tanvirulislam345 <tanvir.topader345@gmail.com>
2025-11-27 10:06:05 +0600

    Description added in this Binary search

--- search_algorithms/binary_search.js
+++ search_algorithms/binary_search.js
@@ -1,5 +1,19 @@
 // Big O Notation: O(log n) - Binary Search Algorithm Because it divides the search space in half with each iteration.
+// When we told log(n) that means that is log2(n)
 // Description: This function performs a binary search on a sorted array to find the index of a target value.
+
+// 1.1 Suppose you have a sorted list of 128 names, and you’re searching
+// through it using binary search. What’s the maximum number of
+// steps it would take?
+
+// 1.2 Suppose you double the size of the list. What’s the maximum
+// number of steps now?
+
+// Every list will take at most log2(n) steps where n is the number of items in the list.
+
+// If the list is 100 items long, it takes at most
+// 7 guesses. If the list is 4 billion items, it takes at most 32 guesses.
+// Example : 100 = 50 => 25 => 13 => 7 => 4 => 2 => 1
 
 let list = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
 
